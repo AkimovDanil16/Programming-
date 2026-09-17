45b6c128545b016fbc668e68a0cc6e8dbb5363cb
Акимов Даниїл <adakimov799@gmail.com>
2026-09-17 18:23:14 +0300

    Лаб 2 була зроблена

--- praktick.py
+++ praktick.py
@@ -1,49 +1,37 @@
-print("Hello, World!")
-b = len('Hello, World')
-c = float(2.5)
-d = ["a", "b", "c", "d"]
-print(d)
-e = dict(a=1, b=2, c=3)
-f = str('Hello, World')
-g = bool(True)
-h = (1, 2, 3)
-print(h)
-i = {1, 2, 3, 4}
-print(i)
-
-a = 10
-b = 2
-print(a + b)
-
-a = 12
-b = 2
-print(a - b)
-
-a = 2
-b = 3
-print(a * b)
-
-a = 10
-b = 2
-print(a / b)
-
-a = 10
-b = 4
-print(a // b)
-
-a = 20
-b = 3
-print(a % b)
-
-a = 15
-b = 12
-print(a ** b)
-
-print(a, type(a))
-print(c, type(c))
-print(f, type(f))
-print(g, type(g))
-print(d, type(d))
-print(h, type(h))
-print(e, type(e))
-print(i, type(i))
+users = {
+    "student1" :{
+        "password" :"1111",
+     "grades": (12, 10, 8, 11, 9, 5, 4)
+    },
+     "student2" :{
+         "password" :"2222",
+        "grades":[8, 7, 11, 12, 10, 6, 11 ]
+     },
+     "student3" :{
+ "password" :"3333",
+    "grades":[7, 9, 7, 6, 5, 10, 11]
+ },
+ "student4" :{
+     "password" :"4444",
+     "grades":[12, 11, 10, 5, 7, 8, 9]
+ }
+}
+login = input("Введіть логін:")
+password = input("Введіть пароль:")
+if login in users and users[login]["password"] == password:
+    grades = users[login]["grades"]
+    print("Вхід виконано успішно!")
+    print("Ваші оцінки")
+    for grade in grades:
+        print(grade)
+    satisfactory = 0
+    unsatisfactory = 0
+    for grade in grades:
+            if 5<= grade <= 12:
+                satisfactory += 1
+            elif 1 <= grade  <=4:
+                unsatisfactory += 1
+    print("Кількість оцінок від 5 до 12:", satisfactory)
+    print("Кількість оцінок від 1 до 4:", unsatisfactory)
+else:
+         print("Неправильний логін або пароль!")
